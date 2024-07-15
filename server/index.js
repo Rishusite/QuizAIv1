b418c106d8d69410cafd90af9c975ec661ffba6e
Rishusite <112302560+Rishusite@users.noreply.github.com>
2024-07-15 18:00:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,9 +14,11 @@ dotenv.config();
 const app=express();
 const port=process.env.PORT || 8000;
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({
+  origin: "*",
+}));
 
 
 //////////////////////////////Database//////////////////////////////////////
