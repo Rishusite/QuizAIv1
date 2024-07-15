905c6e88dcac86a7ae6da246547de9fcab4dcbda
Rishusite <112302560+Rishusite@users.noreply.github.com>
2024-07-15 17:31:38 +0530

    Update Contactus.jsx

--- client/src/components/Contactus.jsx
+++ client/src/components/Contactus.jsx
@@ -24,7 +24,7 @@ const Contactus = () => {
 
   function sendData(e){
     e.preventDefault();
-    axios.post("https://quizappv2-one.vercel.app/contactus",Data).then((response)=>{
+    axios.post("https://quiz-server-nu.vercel.app/contactus",Data).then((response)=>{
       //console.log(response);
       if(response.data===true){
         //console.log('Invalid Username or Password');
